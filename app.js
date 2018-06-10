1d4a2935ede4dafe0b371fcd7eaca59c77001434
adnanrahic <ado.raha198@gmail.com>
2018-06-11 01:36:34 +0200

    now it's kinda working if you disable the read/write filters on the Stitch MongoDB instance

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-// const sls = require('serverless-http')
 const express = require('express')
 const app = express()
 const initDb = require('./db')
@@ -13,19 +12,17 @@ app.post('/', async (req, res, next) => {
 
   const addedNote = await db.collection('notes')
     .findOne({ _id: insertedId })
-  console.log(addedNote)
 
   res.status(200).send(addedNote)
 })
 
 app.put('/:id', async (req, res, next) => {
-  const { db } = await initDb()
+  const { db, ownerId } = await initDb()
   const { insertedId } = await db.collection('notes')
-    .updateOne({ _id: req.params.id }, req.body, { upsert: true })
+    .updateOne({ owner_id: ownerId, _id: req.params.id }, req.body, { upsert: true })
 
   const updatedNote = await db.collection('notes')
     .findOne({ _id: insertedId })
-  console.log(updatedNote)
 
   res.status(200).send(updatedNote)
 })
@@ -35,19 +32,24 @@ app.get('/', async (req, res, next) => {
   const foundNotes = await db.collection('notes')
     .find({})
     .execute()
-  console.log(foundNotes)
 
   res.status(200).send(foundNotes)
 })
 
 app.get('/:id', async (req, res, next) => {
-  const { db } = await initDb()
+  const { db, ObjectId } = await initDb()
   const foundNote = await db.collection('notes')
-    .findOne({ _id: req.params.id })
-  console.log(foundNote)
+    .findOne({ _id: ObjectId(req.params.id) })
 
   res.status(200).send(foundNote)
 })
 
-// module.exports.run = sls(app)
-app.listen(3000)
+app.delete('/:id', async (req, res, next) => {
+  const { db, ObjectId } = await initDb()
+  const deletedNote = await db.collection('notes')
+    .deleteOne({ _id: ObjectId(req.params.id) })
+
+  res.status(200).send(deletedNote)
+})
+
+module.exports = app
--- db.js
+++ db.js
@@ -1,14 +1,27 @@
-const stitch = require('mongodb-stitch')
+const { StitchClientFactory, BSON } = require('mongodb-stitch')
+const { ObjectId } = BSON
+const appId = 'test-stitch-dejsq'
+const database = 'test'
+const connection = {}
 
 module.exports = async () => {
+  console.log(connection.isConnected)
+  if (connection.isConnected) {
+    return connection
+  }
+
   try {
-    const client = await stitch.StitchClientFactory.create('test-stitch-dejsq')
-    const db = client.service('mongodb', 'mongodb-atlas').db('test')
+    const client = await StitchClientFactory.create(appId)
+    const db = client.service('mongodb', 'mongodb-atlas').db(database)
     await client.login()
     const ownerId = client.authedId()
     console.log('[MongoDB Stitch] Connected to Stitch')
 
-    return { db, ownerId }
+    connection.isConnected = true
+    connection.db = db
+    connection.ownerId = ownerId
+    connection.ObjectId = ObjectId
+    return connection
   } catch (err) {
     console.error(err)
   }
