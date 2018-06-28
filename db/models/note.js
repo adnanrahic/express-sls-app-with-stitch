e2c0563b8cbf4641fdd83a48bafd5490d37315b1
adnanrahic <ado.raha198@gmail.com>
2018-06-28 17:40:00 +0200

    fix

--- db/models/note.js
+++ db/models/note.js
@@ -2,7 +2,5 @@ const mongoose = require('mongoose')
 const NoteSchema = new mongoose.Schema({
   title: String,
   description: String
-}, {
-  versionKey: false
 })
 module.exports = mongoose.model('Note', NoteSchema)
