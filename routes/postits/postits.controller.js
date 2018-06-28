b37d3064dcf96767e09373fddc3064372649ea04
adnanrahic <ado.raha198@gmail.com>
2018-06-28 17:27:28 +0200

    fix

--- routes/postits/postits.controller.js
+++ routes/postits/postits.controller.js
@@ -1,49 +1,49 @@
 /* global __root */
 const express = require('express')
-const tasksController = express.Router()
+const postitsController = express.Router()
 const initDb = require(__root + '/db')
 const Note = require(__root + '/db/models/note')
 
-tasksController
+postitsController
   .get('/status', async (req, res, next) => {
     const { db } = await initDb()
     console.log(db)
     res.status(200).send('OK')
   })
 
-tasksController
+postitsController
   .post('/', async (req, res, next) => {
     await initDb()
     const note = await Note.create(req.body)
     res.status(200).send(note)
   })
 
-tasksController
+postitsController
   .put('/:id', async (req, res, next) => {
     await initDb()
     const note = await Note.findByIdAndUpdate(req.params.id, { $set: req.body }, { $upsert: true })
     res.status(200).send(note)
   })
 
-tasksController
+postitsController
   .get('/', async (req, res, next) => {
     await initDb()
     const notes = await Note.find()
     res.status(200).send(notes)
   })
 
-tasksController
+postitsController
   .get('/:id', async (req, res, next) => {
     await initDb()
     const note = await Note.findById(req.params.id)
     res.status(200).send(note)
   })
 
-tasksController
+postitsController
   .delete('/:id', async (req, res, next) => {
     await initDb()
     const note = await Note.deleteOne({ _id: req.params.id })
     res.status(200).send(note)
   })
 
-module.exports = tasksController
+module.exports = postitsController
